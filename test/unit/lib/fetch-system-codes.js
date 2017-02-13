4b1e219a774c1f01b09f7d805b9870a6c2730084
Rowan Manning <accounts@rowanmanning.co.uk>
2017-02-13 16:37:38 +0000

    Add CMDB checking (#1)
    * Add CMDB checking
    
    * Log a warning if a system code can't be verified

---
+++ test/unit/lib/fetch-system-codes.js
@@ -0,0 +1,200 @@
+'use strict';
+
+const assert = require('proclaim');
+const mockery = require('mockery');
+
+describe('lib/middleware/fetch-system-codes', () => {
+	let fetchSystemCodes;
+	let httpError;
+	let request;
+
+	beforeEach(() => {
+		httpError = require('../mock/http-errors.mock');
+		mockery.registerMock('http-errors', httpError);
+
+		request = require('../mock/request.mock');
+		mockery.registerMock('request', request);
+
+		fetchSystemCodes = require('../../../lib/fetch-system-codes');
+	});
+
+	it('exports a function', () => {
+		assert.isFunction(fetchSystemCodes);
+	});
+
+	describe('fetchSystemCodes(apiKey)', () => {
+		let mockResponse1;
+		let mockResponse2;
+		let mockResponse3;
+		let resolvedValue;
+		let returnedPromise;
+
+		beforeEach(() => {
+
+			mockResponse1 = {
+				statusCode: 200,
+				headers: {
+					count: 'Pages: 3, Items: 4'
+				},
+				body: [
+					{value: 'foo'},
+					{value: 'bar'}
+				]
+			};
+			mockResponse2 = {
+				statusCode: 200,
+				headers: {
+					count: 'Pages: 3, Items: 4'
+				},
+				body: [
+					{value: 'baz'}
+				]
+			};
+			mockResponse3 = {
+				statusCode: 200,
+				headers: {
+					count: 'Pages: 3, Items: 4'
+				},
+				body: [
+					{value: 'qux'}
+				]
+			};
+			request.onCall(0).yields(null, mockResponse1);
+			request.onCall(1).yields(null, mockResponse2);
+			request.onCall(2).yields(null, mockResponse3);
+
+			return returnedPromise = fetchSystemCodes('mock-api-key').then(value => {
+				resolvedValue = value;
+			});
+		});
+
+		it('returns a Promise', () => {
+			assert.instanceOf(returnedPromise, Promise);
+		});
+
+		it('calls `request` for each page of system codes', () => {
+			assert.calledThrice(request);
+			assert.calledWith(request.getCall(0), {
+				json: true,
+				url: 'https://cmdb.ft.com/v2/itemattributes/?attributeType=systemCode&apikey=mock-api-key&page=1'
+			});
+			assert.calledWith(request.getCall(1), {
+				json: true,
+				url: 'https://cmdb.ft.com/v2/itemattributes/?attributeType=systemCode&apikey=mock-api-key&page=2'
+			});
+			assert.calledWith(request.getCall(2), {
+				json: true,
+				url: 'https://cmdb.ft.com/v2/itemattributes/?attributeType=systemCode&apikey=mock-api-key&page=3'
+			});
+		});
+
+		it('resolves with an array of system codes', () => {
+			assert.isArray(resolvedValue);
+			assert.deepEqual(resolvedValue, [
+				'foo',
+				'bar',
+				'baz',
+				'qux'
+			]);
+		});
+
+		describe('when a `Count` header is not present in the response', () => {
+
+			beforeEach(() => {
+				request.reset();
+				delete mockResponse1.headers.count;
+				return returnedPromise = fetchSystemCodes('mock-api-key').then(value => {
+					resolvedValue = value;
+				});
+			});
+
+			it('calls `request` once', () => {
+				assert.calledOnce(request);
+				assert.calledWith(request.getCall(0), {
+					json: true,
+					url: 'https://cmdb.ft.com/v2/itemattributes/?attributeType=systemCode&apikey=mock-api-key&page=1'
+				});
+			});
+
+			it('resolves with an array of system codes', () => {
+				assert.isArray(resolvedValue);
+				assert.deepEqual(resolvedValue, [
+					'foo',
+					'bar'
+				]);
+			});
+
+		});
+
+		describe('when the `Count` header in the response is malformed', () => {
+
+			beforeEach(() => {
+				request.reset();
+				mockResponse1.headers.count = 'Pages: one, Items: 2';
+				return returnedPromise = fetchSystemCodes('mock-api-key').then(value => {
+					resolvedValue = value;
+				});
+			});
+
+			it('calls `request` once', () => {
+				assert.calledOnce(request);
+				assert.calledWith(request.getCall(0), {
+					json: true,
+					url: 'https://cmdb.ft.com/v2/itemattributes/?attributeType=systemCode&apikey=mock-api-key&page=1'
+				});
+			});
+
+			it('resolves with an array of system codes', () => {
+				assert.isArray(resolvedValue);
+				assert.deepEqual(resolvedValue, [
+					'foo',
+					'bar'
+				]);
+			});
+
+		});
+
+		describe('when `request` errors', () => {
+			let requestError;
+			let caughtError;
+
+			beforeEach(() => {
+				request.reset();
+				requestError = new Error('request error');
+				request.onCall(0).yields(requestError);
+				return returnedPromise = fetchSystemCodes('mock-api-key').catch(error => {
+					caughtError = error;
+				});
+			});
+
+			it('rejects with the request error', () => {
+				assert.strictEqual(caughtError, requestError);
+			});
+
+		});
+
+		describe('when the CMDB responds with a non-200 code', () => {
+			let caughtError;
+
+			beforeEach(() => {
+				request.reset();
+				mockResponse1.statusCode = 400;
+				return returnedPromise = fetchSystemCodes('mock-api-key').catch(error => {
+					caughtError = error;
+				});
+			});
+
+			it('creates an HTTP error', () => {
+				assert.calledOnce(httpError);
+				assert.calledWithExactly(httpError, mockResponse1.statusCode);
+			});
+
+			it('rejects with the HTTP error', () => {
+				assert.strictEqual(caughtError, httpError.mockError);
+			});
+
+		});
+
+	});
+
+});
--- test/unit/lib/source-param-middleware.js
+++ test/unit/lib/source-param-middleware.js
@@ -7,19 +7,32 @@ const sinon = require('sinon');
 describe('lib/middleware/require-source-param', () => {
 	let defaults;
 	let express;
+	let fetchSystemCodes;
 	let httpError;
+	let log;
 	let sourceParam;
 
 	beforeEach(() => {
+		sinon.stub(global, 'setInterval');
+
 		defaults = sinon.spy(require('lodash/defaults'));
 		mockery.registerMock('lodash/defaults', defaults);
 
 		express = require('../mock/express.mock');
 
+		fetchSystemCodes = require('../mock/fetch-system-codes.mock');
+		mockery.registerMock('./fetch-system-codes', fetchSystemCodes);
+
 		httpError = require('../mock/http-errors.mock');
 		mockery.registerMock('http-errors', httpError);
 
+		log = require('../mock/log.mock');
+
 		sourceParam = require('../../../lib/source-param-middleware');
+	});
+
+	afterEach(() => {
+		global.setInterval.restore();
 	});
 
 	it('exports a function', () => {
@@ -32,8 +45,24 @@ describe('lib/middleware/require-source-param', () => {
 
 	describe('.defaults', () => {
 
+		it('has a `cmdbApiKey` property', () => {
+			assert.isNull(sourceParam.defaults.cmdbApiKey);
+		});
+
 		it('has an `errorMessage` property', () => {
-			assert.deepEqual(sourceParam.defaults.errorMessage, 'The source parameter is required and should be a valid system code');
+			assert.deepEqual(sourceParam.defaults.errorMessage, 'The source parameter is required and must be a valid system code');
+		});
+
+		it('has a `log` property', () => {
+			assert.strictEqual(sourceParam.defaults.log, console);
+		});
+
+		it('has a `pollInterval` property', () => {
+			assert.strictEqual(sourceParam.defaults.pollInterval, 300000);
+		});
+
+		it('has a `verifyUsingCmdb` property', () => {
+			assert.isTrue(sourceParam.defaults.verifyUsingCmdb);
 		});
 
 	});
@@ -43,8 +72,15 @@ describe('lib/middleware/require-source-param', () => {
 		let options;
 
 		beforeEach(() => {
+			fetchSystemCodes.resolves([
+				'test',
+				'valid'
+			]);
 			options = {
-				errorMessage: 'mock-error-message'
+				cmdbApiKey: 'mock-api-key',
+				errorMessage: 'mock-error-message',
+				log: log,
+				pollInterval: 123
 			};
 			middleware = sourceParam(options);
 		});
@@ -56,31 +92,72 @@ describe('lib/middleware/require-source-param', () => {
 			assert.strictEqual(defaults.firstCall.args[2], sourceParam.defaults);
 		});
 
+		it('sets an interval', () => {
+			assert.calledOnce(setInterval);
+			assert.isFunction(setInterval.firstCall.args[0]);
+			assert.strictEqual(setInterval.firstCall.args[1], options.pollInterval);
+		});
+
+		describe('interval()', () => {
+			let interval;
+
+			beforeEach(() => {
+				fetchSystemCodes.reset();
+				interval = setInterval.firstCall.args[0];
+				interval();
+			});
+
+			it('calls `fetchSystemCodes` with the API key', () => {
+				assert.calledOnce(fetchSystemCodes);
+				assert.calledWithExactly(fetchSystemCodes, options.cmdbApiKey);
+			});
+
+			describe('when `fetchSystemCodes` errors', () => {
+				let fetchError;
+
+				beforeEach(() => {
+					fetchError = new Error('fetch error');
+					fetchSystemCodes.rejects(fetchError);
+					return interval();
+				});
+
+				it('logs the error', () => {
+					assert.called(options.log.error);
+					assert.calledWithExactly(options.log.error, 'CMDB error: fetch error');
+				});
+
+			});
+
+		});
+
 		it('returns a middleware function', () => {
 			assert.isFunction(middleware);
 		});
 
 		describe('middleware(request, response, next)', () => {
-			let next;
+			let middlewareError;
 
-			beforeEach(() => {
-				next = sinon.spy();
+			beforeEach(done => {
 				express.mockRequest.query.source = 'test';
-				middleware(express.mockRequest, express.mockResponse, next);
+				middleware(express.mockRequest, express.mockResponse, error => {
+					middlewareError = error;
+					done();
+				});
 			});
 
-			it('calls `next` with no error', () => {
-				assert.calledOnce(next);
-				assert.calledWithExactly(next);
+			it('calls back with no error', () => {
+				assert.isUndefined(middlewareError);
 			});
 
 			describe('when the `source` query parameter is missing', () => {
 
-				beforeEach(() => {
-					next.reset();
+				beforeEach(done => {
 					httpError.reset();
 					delete express.mockRequest.query.source;
-					middleware(express.mockRequest, express.mockResponse, next);
+					middleware(express.mockRequest, express.mockResponse, error => {
+						middlewareError = error;
+						done();
+					});
 				});
 
 				it('creates a 400 HTTP error with `options.errorMessage`', () => {
@@ -88,20 +165,21 @@ describe('lib/middleware/require-source-param', () => {
 					assert.calledWithExactly(httpError, 400, options.errorMessage);
 				});
 
-				it('calls `next` with the created error', () => {
-					assert.calledOnce(next);
-					assert.calledWithExactly(next, httpError.mockError);
+				it('calls back with the created error', () => {
+					assert.strictEqual(middlewareError, httpError.mockError);
 				});
 
 			});
 
 			describe('when the `source` query parameter is an empty string', () => {
 
-				beforeEach(() => {
-					next.reset();
+				beforeEach(done => {
 					httpError.reset();
 					express.mockRequest.query.source = '';
-					middleware(express.mockRequest, express.mockResponse, next);
+					middleware(express.mockRequest, express.mockResponse, error => {
+						middlewareError = error;
+						done();
+					});
 				});
 
 				it('creates a 400 HTTP error with `options.errorMessage`', () => {
@@ -109,20 +187,21 @@ describe('lib/middleware/require-source-param', () => {
 					assert.calledWithExactly(httpError, 400, options.errorMessage);
 				});
 
-				it('calls `next` with the created error', () => {
-					assert.calledOnce(next);
-					assert.calledWithExactly(next, httpError.mockError);
+				it('calls back with the created error', () => {
+					assert.strictEqual(middlewareError, httpError.mockError);
 				});
 
 			});
 
 			describe('when the `source` query parameter is longer than 255 characters', () => {
 
-				beforeEach(() => {
-					next.reset();
+				beforeEach(done => {
 					httpError.reset();
 					express.mockRequest.query.source = Array(256).fill('x').join('');
-					middleware(express.mockRequest, express.mockResponse, next);
+					middleware(express.mockRequest, express.mockResponse, error => {
+						middlewareError = error;
+						done();
+					});
 				});
 
 				it('creates a 400 HTTP error with `options.errorMessage`', () => {
@@ -130,9 +209,100 @@ describe('lib/middleware/require-source-param', () => {
 					assert.calledWithExactly(httpError, 400, options.errorMessage);
 				});
 
-				it('calls `next` with the created error', () => {
-					assert.calledOnce(next);
-					assert.calledWithExactly(next, httpError.mockError);
+				it('calls back with the created error', () => {
+					assert.strictEqual(middlewareError, httpError.mockError);
+				});
+
+			});
+
+			describe('when the `source` query parameter is not found in CMDB', () => {
+
+				beforeEach(done => {
+					httpError.reset();
+					express.mockRequest.query.source = 'invalid';
+					middleware(express.mockRequest, express.mockResponse, error => {
+						middlewareError = error;
+						done();
+					});
+				});
+
+				it('creates a 400 HTTP error with `options.errorMessage`', () => {
+					assert.calledOnce(httpError);
+					assert.calledWithExactly(httpError, 400, options.errorMessage);
+				});
+
+				it('calls back with the created error', () => {
+					assert.strictEqual(middlewareError, httpError.mockError);
+				});
+
+			});
+
+		});
+
+		describe('when `options.verifyUsingCmdb` is `false`', () => {
+
+			beforeEach(() => {
+				setInterval.reset();
+				options.verifyUsingCmdb = false;
+				middleware = sourceParam(options);
+			});
+
+			it('does not set an interval', () => {
+				assert.notCalled(setInterval);
+			});
+
+			describe('middleware(request, response, next)', () => {
+				let middlewareError;
+
+				describe('when the `source` query parameter is not found in CMDB', () => {
+
+					beforeEach(done => {
+						express.mockRequest.query.source = 'invalid';
+						middleware(express.mockRequest, express.mockResponse, error => {
+							middlewareError = error;
+							done();
+						});
+					});
+
+					it('calls back with no error', () => {
+						assert.isUndefined(middlewareError);
+					});
+
+				});
+
+			});
+
+		});
+
+		describe('when fetching system codes has no response yet', () => {
+
+			beforeEach(() => {
+				fetchSystemCodes.resolves();
+				middleware = sourceParam(options);
+			});
+
+			describe('middleware(request, response, next)', () => {
+				let middlewareError;
+
+				describe('when the `source` query parameter is not found in CMDB', () => {
+
+					beforeEach(done => {
+						express.mockRequest.query.source = 'invalid';
+						middleware(express.mockRequest, express.mockResponse, error => {
+							middlewareError = error;
+							done();
+						});
+					});
+
+					it('logs that the system code cannot be verified', () => {
+						assert.called(options.log.warn);
+						assert.calledWithExactly(options.log.warn, 'CMDB warning: The system code "invalid" cannot be verified due to a CMDB failure');
+					});
+
+					it('calls back with no error', () => {
+						assert.isUndefined(middlewareError);
+					});
+
 				});
 
 			});
